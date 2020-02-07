53690d70d2f729114e1d27eae0fcf900efc3ab9a
QuangMinh <quangminh.lenguyen.si@gmail.com>
2020-02-07 14:12:28 +0100

    hook : DateDisplay v3 - React.memo + hook useCallback

--- src/hook/DateDisplay.js
+++ src/hook/DateDisplay.js
@@ -1,17 +1,24 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
 
 /*Explanation :
     - 1e render -> effect
     - 2e render -> clean effect -> new effect
     - 3e render -> clean effect -> new effect
 */
+
+// React.memo = PureComponent in ClassComponent
+const Button = React.memo(function({onClick, children}) {
+    console.log('render Button');
+    return <button onClick={onClick}>{children}</button>
+});
+
 function DateDisplay() {
     const [play, setPlay] = useState(true);
     const [date, setDate] = useState(new Date());
 
     useEffect(() => {
-        console.log('run effect only when play is changed');
         if (play) {
+            console.log('set newInterval');
             const interval = setInterval(() => setDate(new Date()), 1000);
 
             return function() {
@@ -21,17 +28,19 @@ function DateDisplay() {
         }
     }, [play]);
 
-    function handleClick() {
-        setPlay(!play);
-        setDate(new Date());
-    }
+    const handleClickPlay = useCallback(
+        function() {
+            console.log('value play in callback handleClickPlay', play);
+            setPlay(!play);
+            setDate(new Date());
+        },
+        [play]
+    );
 
-    console.warn('render');
+    console.warn('render DateDisplay');
     return (
         <div>
-            <button onClick={handleClick}>
-                {play ? 'Pause' : 'Play'}
-            </button>
+            <Button onClick={handleClickPlay}>{play ? 'Pause' : 'Play'}</Button>
             <p>
                 Nous sommes le {date.toLocaleString("fr-FR")}
             </p>
