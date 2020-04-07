46e15e0c00e0d113a8e8e5aa18337cbd60b69b45
QuangMinh <quangminh.lenguyen.si@gmail.com>
2020-04-07 16:24:23 +0200

    reducer-egghead : connect of React Redux

--- src/redux/reducers/notesReducer.js
+++ src/redux/reducers/notesReducer.js
@@ -1,6 +1,6 @@
 import {ADD_NOTE, REMOVE_NOTE} from "../actions/actions";
 
-// what is reducer ? : it' a function with the arguments : (prevState, action) which return nextState
+// what is reducer ? : it' a function with the arguments : (state, action) which return nextState
 const notesReducer = (notes = [], {type, payload}) => {
     switch (type) {
         case ADD_NOTE:
